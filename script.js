06a099cc92939fb45f2a0a8e095a32b3715e3bbe
Jpfpp12 <118419073+Jpfpp12@users.noreply.github.com>
2025-03-12 12:27:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function insert_Row() {
     let cell1 = newRow.insertCell(0);
     let cell2 = newRow.insertCell(1);
 
-    cell1.innerHTML = "New cell1";
-    cell2.innerHTML = "New cell2";
+    cell1.innerHTML = "New Cell1";
+    cell2.innerHTML = "New Cell2";
 }
 
